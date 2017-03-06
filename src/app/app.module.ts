1987f2813f2f384e631c0c0e3439fd8b19d3099c
hodys <odyody12@gmail.com>
2017-03-06 19:29:05 +0900

    chore: component 통합

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchBoxComponent } from './search-box/search-box.component';
-import { TabComponent } from './tab/tab.component';
-import { TabContentComponent } from './tab-content/tab-content.component';
-import { NewsListComponent } from './news-list/news-list.component';
-import { HotKeywordComponent } from './hot-keyword/hot-keyword.component';
-import { PhotoNewsSlidesComponent } from './photo-news-slides/photo-news-slides.component';
-import { WeatherComponent } from './weather/weather.component';
+import { HeaderComponent } from './content/header/header.component';
+import { SearchBoxComponent } from './content/search-box/search-box.component';
+import { TabComponent } from './content/tab/tab.component';
+import { TabContentComponent } from './content/tab-content/tab-content.component';
+import { NewsListComponent } from './content/news-list/news-list.component';
+import { HotKeywordComponent } from './content/hot-keyword/hot-keyword.component';
+import { PhotoNewsSlidesComponent } from './content/photo-news-slides/photo-news-slides.component';
+import { WeatherComponent } from './content/weather/weather.component';
 import { ApiModule } from './api/api.module';
 
 @NgModule({
